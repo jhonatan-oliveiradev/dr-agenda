4712658a4870fcf28b46621eed35d2235c064a51
jhonatan-oliveiradev <jhonatan.oliveira931002@gmail.com>
2025-05-30 17:24:14 -0300

    feat: add clinic form page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,18 +1,34 @@
-import {headers} from 'next/headers'
+import { eq } from "drizzle-orm";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-import { auth } from "@/lib/auth"
+import { db } from "@/db";
+import { usersToClinicsTable } from "@/db/schema";
+import { auth } from "@/lib/auth";
 
-import { SignOutButton } from './components/sign-out-button';
+import { SignOutButton } from "./components/sign-out-button";
 
 export default async function DashboardPage() {
   const session = await auth.api.getSession({
-    headers: await headers()
+    headers: await headers(),
   });
+
+  if (!session?.user) {
+    redirect("/authentication");
+  }
+
+  const clinics = await db.query.usersToClinicsTable.findMany({
+    where: eq(usersToClinicsTable.userId, session.user.id),
+  });
+
+  if (clinics.length === 0) {
+    redirect("/clinic-form");
+  }
 
   return (
     <div>
       <h1>{session?.user?.name}</h1>
-     <SignOutButton />
-  </div>
-  )
+      <SignOutButton />
+    </div>
+  );
 }
